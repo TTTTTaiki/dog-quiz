819f1ba9b7a97acb45a4a9799e4369585513576e
TTTTTaiki <itoutaiki418@gmail.com>
2025-07-01 23:58:54 +0900

    Bulmaで遊んだ

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,25 @@ import { fetchImages } from "./api";
 
 function Header() {
   return (
-    <header>
-      <h1>What dog breed is this picture?</h1>
-    </header>
+    <div>
+      <header className="hero is-dark is-small">
+        <div className="hero-body">
+          <div className="title">
+            <h1>What dog breed is this picture?</h1>
+          </div>
+        </div>
+      </header>
+    </div>
   );
 }
 
 function Image(props) {
   return (
-    <figure>
-      <img src={props.src} alt="cute dog!" />
-    </figure>
+    <div>
+      <figure className="image is128x128">
+        <img src={props.src} alt="cute dog!" />
+      </figure>
+    </div>
   )
 }
 
@@ -80,13 +88,15 @@ function Main() {
   }
   return (
     <main>
-      <section>
+      <section className="section">
         <div>
           <Form onFormSubmit={reloadImages} />
         </div>
       </section>
-      <section>
-        <Gallery src={urls} />
+      <section className="section">
+        <div className="container">
+          <Gallery src={urls} />
+        </div>
       </section>
     </main>
   );
@@ -95,12 +105,16 @@ function Main() {
 function Footer() {
   const url = "https://dog.ceo/dog-api/about";
   return (
-    <footer>
-        <p>Dog images are retrieved from Dog API</p>
-        <p>
-          <a href={url}>Donate to Dog API</a>
-        </p>
+    <div>
+      <footer className="footer">
+        <div className="content has-text-centered">
+          <p>Dog images are retrieved from Dog API</p>
+          <p>
+            <a href={url}>Donate to Dog API</a>
+          </p>
+        </div>
       </footer>
+    </div>
   );
 }
 
